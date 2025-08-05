c769334bbf76705459c1f089930b4621aea4fe6d
Ramil Gallyamov <gallyamow@gmail.com>
2025-08-05 23:12:12 +0300

    parser

--- internal/crawler/parser.go
+++ internal/crawler/parser.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"golang.org/x/net/html"
+	"net/url"
 )
 
 type ParseResult struct {
@@ -28,27 +29,50 @@ func (p *Parser) Parse(page *Page) (*ParseResult, error) {
 		return nil, fmt.Errorf("parse html: %w", err)
 	}
 
-	links := extractLinks(node)
 	return &ParseResult{
-		Links: links,
+		Links: extractLinks(page.URL, node),
 	}, nil
 }
 
-func extractLinks(node *html.Node) []string {
+func extractLinks(currentURL string, node *html.Node) []string {
 	var links []string
 
 	if node.Type == html.ElementNode && node.Data == "a" {
 		for _, attr := range node.Attr {
 			if attr.Key == "href" && attr.Val != "" {
-				links = append(links, attr.Val)
+				href := attr.Val
+
+				switch {
+				case href == "/":
+				case href == "#":
+				case href[0] == '#':
+					// skip
+					continue
+				default:
+					links = append(links, resolveAbsoluteUrl(currentURL, href))
+				}
 			}
 		}
 	}
 
 	// recursive walk
 	for c := node.FirstChild; c != nil; c = c.NextSibling {
-		links = append(links, extractLinks(c)...)
+		links = append(links, extractLinks(currentURL, c)...)
 	}
 
 	return links
+}
+
+func resolveAbsoluteUrl(currentUrl string, path string) string {
+	base, err := url.Parse(currentUrl)
+	if err != nil {
+		return currentUrl + path
+	}
+
+	ref, err := url.Parse(path)
+	if err != nil {
+		return currentUrl + path
+	}
+
+	return base.ResolveReference(ref).String()
 }
