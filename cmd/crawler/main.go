c39f2ab471e0f51f678279a5712a0a3738eeff44
Ramil Gallyamov <gallyamow@gmail.com>
2025-08-08 17:51:28 +0300

    wrong way

--- cmd/crawler/main.go
+++ cmd/crawler/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"go-crawler/internal/crawler"
 	"log/slog"
 	"os"
@@ -12,7 +13,7 @@ func main() {
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 
 	maxCount := 10
-	maxConcurrent := 1
+	maxConcurrent := 5
 	startUrl := "https://go.dev/learn/"
 
 	fetcher := crawler.NewFetcher()
@@ -44,42 +45,49 @@ func main() {
 	startedAt := time.Now()
 	sem := crawler.NewSemaphore(maxConcurrent)
 	wg := sync.WaitGroup{}
+	visited := make(map[string]struct{})
 	queue := []string{startUrl}
 	cnt := 0
 
+	wg.Add(1) // ждем цикл
 	for len(queue) > 0 {
-		url := queue[0]
+		queuedURL := queue[0]
 		queue = queue[1:]
 
-		wg.Add(1)
-		go func() {
+		go func(u string) {
+			fmt.Println("DDDDDDDDDDDDDDD" + u)
 			defer wg.Done()
 
 			sem.Acquire()
 			defer sem.Release()
 
-			parsed, saved, err := handler(url)
+			parsed, saved, err := handler(u)
 			if err != nil {
-				logger.Error("Failed to handle", "err", err, "url", url)
+				logger.Error("Failed to handle", "err", err, "url", u)
 				return
 			}
 
-			logger.Info("Successfully handled", "url", url, "parsed", parsed.String(), "saved", saved.String())
+			logger.Info("Successfully handled", "url", u, "parsed", parsed.String(), "saved", saved.String())
 			cnt += 1
 
-			// TODO: обработать дубли
-			queue = append(queue, parsed.Links...)
-		}()
+			for _, link := range parsed.Links {
+				if _, ok := visited[link]; ok {
+					continue
+				}
+
+				visited[link] = struct{}{}
+				queue = append(queue, link)
+			}
+		}(queuedURL)
 
 		if cnt >= maxCount {
-			logger.Info("Limit exceed", "limit", maxCount)
+			logger.Info("Page limit exceed", "limit", maxCount)
 			break
 		}
 
+		// приходится ждать в цикле, без этого срабатывает только для одной записи
 		wg.Wait()
 	}
-
-	wg.Wait()
 
 	logger.Info("Elapsed time", "time", time.Since(startedAt).String())
 }
